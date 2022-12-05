f5259f8397165345d03db4af1c3c6c5789b80ce0
Kali <tavarescauanf@gmail.com>
2022-12-04 23:44:01 -0300

    Página de update

--- app.py
+++ app.py
@@ -1,46 +1,103 @@
 from flask import Flask, render_template, request
+import time
 import csv
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    # templates/home.html
-    tasks = take_tasks()
-    print('Log1', tasks)
-    return render_template('home.html', tasks=tasks)
+    return show_products()
 
 @app.route('/create', methods=['POST'])
 def create():
-    name = request.form['name']
-    task = {'name': name, 'finished': False}
-    write_task(task)
-    tasks = take_tasks()
-    return render_template('home.html', tasks=tasks)
-
-@app.route("/bye")
-def bye():
-    return 'Bye!'
-
-def take_tasks():
     
-    file_in = open('tasks.json', 'rt' )
-    conteudo = file_in.read()
-    conteudo = json.loads(conteudo)
-    file_in.close()
+    nome = request.form['nome']         
+    quantidade = request.form['quantidade'] 
+    preco = request.form['preco']
+    id = int(time.time() * 1000)
 
-    return conteudo
+    with open('produtos.csv', 'a', newline = '') as file_out:
+        fieldnames = ['Id', 'Nome','Quantidade','Preco']
+        writer = csv.DictWriter(file_out, fieldnames=fieldnames)
 
-def write_task(task):
+        writer.writerow({'Id': id, 'Nome': nome, 'Quantidade': quantidade, 'Preco': preco})
 
-    conteudo = take_tasks()
+    return home()
 
-    print('LOG1', conteudo)
+@app.route('/delete/<id>')
+def delete(id):
 
-    file_out = open('tasks.json', 'wt' )
-    add = conteudo.append(task)
-    print('LOG2', add)
-    file_out.write(add)
-    file_out.close()
+    produtos_list = []
 
-app.run(debug = True)
+    with open('produtos.csv', 'rt') as file_in:
+        produtos = csv.DictReader(file_in)
+        for produto in produtos:
+            produtos_list.append(produto)
+
+    print('produtos_list', produtos_list)           
+
+    with open('produtos.csv', 'wt', newline='') as file_out:
+
+        fieldnames = ['Id', 'Nome','Quantidade','Preco']
+        writer = csv.DictWriter(file_out, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer = csv.writer(file_out)
+        for produto in produtos_list:
+            if produto['Id'] != id:
+                print('Produto dif', f"{produto['Id']},{produto['Nome']},{produto['Quantidade']},{produto['Preco']}")
+                writer.writerow([produto['Id'], produto['Nome'], produto['Quantidade'], produto['Preco']])
+    
+    return home()
+    
+
+@app.route('/update/<id>/<nome>/<quantidade>/<preco>')
+def update(id, nome, quantidade, preco):
+    lista = {
+        "id": id,
+        "nome": nome,
+        "quantidade": quantidade,
+        "preco": preco
+    }
+
+    return render_template('update.html', lista=lista)
+
+
+#salva os forms que foram modificados do /update/
+@app.route('/saveup', methods=['POST'])
+def saveup():
+
+    #obtem as novas variaveis
+    id = request.form['id'] # o id esta ocultado na pagina
+    nome = request.form['nome']         
+    data = request.form['data'] 
+    preco = request.form['preco']
+
+    #abre o dataframe do .csv
+    data = pd.read_csv("compras.csv")
+
+    #cria um novo dataframe apartir das novas variaveis
+    new_df = pd.DataFrame({'Id': [id],'Nome': [nome],'Data': [dat],'Preco': [preco]})
+
+    #seta os index's para a coluna 'Id'
+    #n sei se isso é necessario mas na minha mente faz sentido 
+    data = data.set_index("Id")
+    new_df = new_df.set_index("Id")
+
+    #atualiza os dados do data frame antigo com o novo
+    data.update(new_df)
+
+    #salva o arquivo
+    data.to_csv('compras.csv')
+
+    #redireciona para "/"
+    with open('compras.csv', 'rt') as file_in:
+        compras = csv.DictReader(file_in)
+        return render_template('index.html', compras=compras)
+
+def show_products():
+    with open('produtos.csv', 'rt') as file_in:
+        produtos = csv.DictReader(file_in)
+        return render_template('home.html', produtos=produtos)
+
+app.run(debug=True)
--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-import csv
+import time
 
-def take_tasks():
-    
-    with open('tasks.csv', 'rt') as file_in:
-        leitor = csv.reader(file_in)
-        print(leitor)
-        for linha in leitor:
-            print(linha)
-
-    return leitor
-
-a = take_tasks()
-for b in a:
-    print(b)
+print(int(time.time() * 1000))
